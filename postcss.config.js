6d5d02392736da05b498c6f702b16eb5ec43d69a
spawnleon@gmail.com <leonov@beehive-software.com>
2019-11-19 17:50:57 +0300

    back

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,15 @@
 module.exports = (env, argv) => {
-  if (env.webpack.mode === 'production') {
+  if (env.webpack.mode === "production") {
     return {
       plugins: [
-        require('autoprefixer')({
-          browsers: ['ie >= 8', 'last 4 version']
+        require("autoprefixer")({
+          browsers: ["ie >= 8", "last 4 version"]
         }),
-        require('cssnano')
+        require("cssnano")
       ]
-    }
+    };
   }
-  if (env.webpack.mode === 'development') {
+  if (env.webpack.mode === "development") {
     return {};
   }
 };
