bb8a7821d533d4228cc132cad7ea0020209c10fa
spawnleon@gmail.com <leonov@beehive-software.com>
2019-12-10 12:14:02 +0300

    fix bugs

--- webpack.config.js
+++ webpack.config.js
@@ -137,6 +137,16 @@ module.exports = (env, options) => {
           }
         }),
       ],
+      // splitChunks: {
+      //   cacheGroups: {
+      //     vendor: {
+      //       name: 'vendors',
+      //       test: /node_modules/,
+      //       chunks: 'all',
+      //       enforce: true
+      //     }
+      //   }
+      // }
     },
     plugins: [
       new MiniCssExtractPlugin({
